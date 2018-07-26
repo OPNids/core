99ef699dce3972b9b41896c1bab40116873d5b8c
Max Orelus <mo@counterflowai.com>
2018-07-26 11:31:06 -0400

    split out rc command

--- src/opnsense/scripts/OPNids/redis/setup.sh
+++ src/opnsense/scripts/OPNids/redis/setup.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-REDIS_DIRS="/var/log/redis"
 
-for REDIS_DIR in ${REDIS_DIRS}; do
-        mkdir -p ${REDIS_DIR}
-        chown -R redis:redis ${REDIS_DIR}
-        chmod -R 0755 ${REDIS_DIR}
-done
-
-sleep 2
-/usr/local/etc/rc.d/redis start
+mkdir -p /var/log/redis
+chown -R redis:redis /var/log/redis
+chmod -R 0755 /var/log/redis

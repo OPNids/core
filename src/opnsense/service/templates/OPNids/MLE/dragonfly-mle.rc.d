d8157b34f53e7251a38479cbb784e778b694af09
Max Orelus <mo@counterflowai.com>
2018-10-26 14:59:29 +0000

    default behavior mle, filebeat -- redis running hidden

--- src/opnsense/service/templates/OPNids/MLE/dragonfly-mle.rc.d
+++ src/opnsense/service/templates/OPNids/MLE/dragonfly-mle.rc.d
@@ -1,4 +1,3 @@
-{{OPNids.MLE.general.enabled}}
 {% if helpers.exists('OPNids.MLE') %}
 {% if helpers.exists('OPNids.MLE.general') and OPNids.MLE.general.enabled|default('0') == '1' %}
 # REQUIRE: redis
